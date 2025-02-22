a11d6534c768f321613b25eb92fe313f461b697c
Sócrates López <josesocrateslopez@gmail.com>
2025-02-21 22:35:44 -0600

    feat: add initial page structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CharactersComponent } from './pages/characters/characters.component';
+import { ThankYouComponent } from './pages/thank-you/thank-you.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [   
+      {
+        path: '',
+        redirectTo: '',
+        pathMatch: 'full'
+      },
+      {
+        path: '',
+        component: MainComponent,
+        children: [
+            {
+                path: 'home',
+                component: HomeComponent
+            },
+            {
+                path: 'characters',
+                component: CharactersComponent
+            },
+            {
+                path: 'thank-you',
+                component: ThankYouComponent
+            }
+        ]
+      },
+      {
+        path: '**',
+        redirectTo: ''
+      }
+];
