8677d1c88b605481c7767d00b518d7ec669576ce
Sócrates López <josesocrateslopez@gmail.com>
2025-02-24 13:32:52 -0600

    fix responsive sidebar

--- src/app/pages/main/components/sidebar/sidebar.component.ts
+++ src/app/pages/main/components/sidebar/sidebar.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { Router, RouterModule } from '@angular/router';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-sidebar',
@@ -15,9 +17,22 @@ import { Router, RouterModule } from '@angular/router';
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
+  @Output() changeDrawerToogle = new EventEmitter<void>();
   selectEmployees: boolean = false
   selectWelcome: boolean = false
-  constructor(private router: Router) {
 
+  constructor(private router: Router, private breakpointObserver: BreakpointObserver) {
+
+  }
+
+  drawerToogle(){
+    this.breakpointObserver.observe(['(max-width: 600px)']).pipe(
+      take(1) 
+    ).subscribe(result => {
+      if (result.matches) {
+        this.changeDrawerToogle.emit()
+      }
+    });
+    
   }
 }
